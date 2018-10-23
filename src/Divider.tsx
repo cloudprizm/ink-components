dbdafd33b71f0455e6c94eb86398b21ed2e467af
Damian Baar <damian.baar@gmail.com>
2018-10-23 09:40:11 +0200

    ink-components/divider: handling measure

--- src/Divider.tsx
+++ src/Divider.tsx
@@ -1,14 +1,17 @@
 import React, { Component } from 'react'
 import { Box, Color } from 'ink'
+import stringWidth from 'string-width'
+
 import { FlexBoxProps } from './Flexbox'
 import { Colors, colorToObject } from './Colors'
 
-export const defaultDividerRenderer = ({ color, title }) =>
+export const defaultDividerRenderer = ({ color, title, char, half }) =>
   <Color {...colorToObject(color)}>
-    <Box>{title}</Box>
+    <Box>{`${char.repeat(half)} ${title} ${char.repeat(half)}`}</Box>
   </Color>
 
 const defaultProps = {
+  char: '-',
   renderDivider: defaultDividerRenderer
 }
 
@@ -21,9 +24,28 @@ interface Props extends DefaultProps, Partial<FlexBoxProps> {
 
 export class Divider extends Component<Props> {
   public static defaultProps = defaultProps
+  public nodeRef
+
+  constructor(props: Props) {
+    super(props)
+    this.nodeRef = React.createRef()
+  }
 
   public render() {
-    const { renderDivider, color, title, ...rest } = this.props
-    return <Box {...rest}>{renderDivider({ color, title })}</Box>
+    const { renderDivider, color, title, char, ...rest } = this.props
+    const max = this.props.width || this.getMaxWidth()
+    const titleWidth = stringWidth(title)
+    const availableSpace = max - titleWidth
+    const half = Math.floor(availableSpace / 2)
+
+    return <div ref={this.nodeRef}>
+      <Box {...rest}>{renderDivider({ color, char, title, half })}</Box>
+    </div>
+  }
+
+  public getMaxWidth() {
+    return this.nodeRef.current
+      ? this.nodeRef.current.yogaNode.getComputedWidth()
+      : process.stdout.columns
   }
 }
